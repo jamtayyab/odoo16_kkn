d8f506a360785d4fc82ad21a6a7de33bc779e45b
jamtayyab <punch.10034@gmail.com>
2023-03-09 15:04:06 +0500

    Archived functionality Added

--- odoo-custom-addons/hospital/models/patient.py
+++ odoo-custom-addons/hospital/models/patient.py
@@ -1,17 +1,22 @@
 from odoo import api, fields, models
 
-class Patient(models.Model):
-    # this will create a table in postgres named "Hospital_Patient"
-   _name = "hospital.patient"  #small case is the convention donot change it
-   _description = "This python data class is related to the Hospital Patient"
 
+class Patient(models.Model):
+
+    # this will create a table in postgres named "Hospital_Patient"
+
+    _name = "hospital.patient"  # small case is the convention don't change it
+    _description = "This python data class is related to the Hospital Patient"
 
     # this will create columns in the table "Hospital_Patient"
     # there will be some magic fields in the table provided by the odoo
-   name = fields.Char(string = "Name")
-   age = fields.Integer(string = "Age")
 
-   #Always make sure tuple are seprated properly.
-   # will have to follow the convention
-   gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
-   room = fields.Selection([("room_a","A Class"), ("room_b", "B Class")],string = "Room")
+    name = fields.Char(string="Name")
+    age = fields.Integer(string="Age")
+
+    # Always make sure tuple are separated properly.
+    # will have to follow the convention
+
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    room = fields.Selection([("room_a", "A Class"), ("room_b", "B Class")], string="Room")
+    active = fields.Boolean(string="Active", default=True)
