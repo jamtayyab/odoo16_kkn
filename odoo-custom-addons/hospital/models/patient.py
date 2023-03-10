2c70ced80ba759c750712ddb86290cfb47bf9da2
jamtayyab <punch.10034@gmail.com>
2023-03-10 12:58:54 +0500

    implemented Computed Fields

--- odoo-custom-addons/hospital/models/patient.py
+++ odoo-custom-addons/hospital/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 
 class Patient(models.Model):
@@ -15,15 +16,26 @@ class Patient(models.Model):
     # there will be some magic fields in the table provided by the odoo
 
     name = fields.Char(string="Name", default="ABC", tracking=True)
-    age = fields.Integer(string="Age", tracking=True)
+    date_of_birth = fields.Date (string = "Date of Birth",tracking=True)
+    age = fields.Integer(string="Age", compute = "age_calculator")
+
 
     # Always make sure tuple are separated properly.
     # will have to follow the convention
 
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True)
     room = fields.Selection([("room_a", "A Class"), ("room_b", "B Class")], string="Room", tracking=True)
-    active = fields.Boolean(string="Active", default=True, tracking=True)
-    active = fields.Boolean(string="Active", default=True, tracking=True)
+    active = fields.Boolean(string="Active", default=True, tracking=True)\
 
+    @api.depends("date_of_birth")
+    def age_calculator(self):
+        today = date.today()
+        # print("This is the data for self: --->  ",self)
+        for i in self:
+            if i.date_of_birth:
+               # print("This is the data for i.date_of_birth: --->  ",i.date_of_birth)
+                i .age = today.year - i.date_of_birth.year
+            else:
+                i.age = 0
 
 
